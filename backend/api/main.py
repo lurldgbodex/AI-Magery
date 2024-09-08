c5c013d6f87f8146986736d22f888844cc40dbce
Lurldgbodex <professionaleditor6@gmail.com>
2024-09-08 21:54:43 +0100

    fix: ci pipeline path

--- backend/api/main.py
+++ backend/api/main.py
@@ -6,7 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     """
-    The default home page url of the api. displays a welcome message when a get request is received.
+    The default home page url of the api.
+    displays a welcome message when a get request is received.
     """
     return {
         "message": "Welcome to the Image Transformation API!"
